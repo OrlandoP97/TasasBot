b2de1b82619d3554ee9c2faa487cabe771613464
Orlando <57908549+OrlandoP97@users.noreply.github.com>
2023-03-31 13:38:49 -0400

    edit rates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import {} from "moment";
 
 const token = "6219172685:AAGQYED-jD08yh3LbnsbewO_En9UgNaqZwo";
 
@@ -24,18 +25,41 @@ bot.on("text", async (ctx) => {
   // Si el mensaje no es un comando, procesa la conversión
   if (!ctx.message.text.startsWith("/")) {
     try {
-      // Obtiene la tasa de cambio actual de CUP a USD desde la API
-      const response = await axios.get("https://tasas.eltoque.com/v1/trmi");
-      const rate = response.data[0].value;
+      const now = new Date();
+      // Formatear la fecha y hora actual en el formato deseado
+      const formattedDateFrom = moment(now)
+        .startOf("day")
+        .format("YYYY-MM-DD 00:00:01");
+      const formattedDateTo = moment(now)
+        .endOf("day")
+        .format("YYYY-MM-DD 23:59:01");
+      // Codificar las fechas formateadas
+      const encodedDateFrom = encodeURIComponent(formattedDateFrom);
+      const encodedDateTo = encodeURIComponent(formattedDateTo);
+      // Construir la URL con los parámetros de las fechas
+      const url = `https://tasas.eltoque.com/v1/trmi?date_from=${encodedDateFrom}&date_to=${encodedDateTo}`;
+      console.log(url);
 
-      // Obtiene el valor ingresado por el usuario
-      const value = parseFloat(ctx.message.text);
+      const config = {
+        headers: {
+          accept: "*/*",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2OTg2ODY5MCwianRpIjoiNjI5NDcxYzAtOWE3NS00YTg3LWI0YmUtNjU1MWI3YmMzN2VlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjYzODgyYzkyZTNhYzlkNWM2NDcwOWM5YyIsIm5iZiI6MTY2OTg2ODY5MCwiZXhwIjoxNzAxNDA0NjkwfQ.KEsO3N7klvVFA6JzoPGV7DxSB-ZGpf-ATYKolcskQDg",
+        },
+      };
 
-      // Convierte el valor de CUP a USD
-      const convertedValue = value / rate;
+      await axios.get(url, config).then((response) => {
+        const rate = response.data[0].tasas.USD;
 
-      // Envía la respuesta al usuario
-      ctx.reply(`${value} CUP = ${convertedValue.toFixed(2)} USD`);
+        // Obtiene el valor ingresado por el usuario
+        const value = parseFloat(ctx.message.text);
+
+        // Convierte el valor de CUP a USD
+        const convertedValue = value / rate;
+
+        // Envía la respuesta al usuario
+        ctx.reply(`${value} CUP = ${convertedValue.toFixed(2)} USD`);
+      });
     } catch (error) {
       // Si hay un error al obtener la tasa de cambio, envía un mensaje de error al usuario
       ctx.reply(
@@ -44,6 +68,5 @@ bot.on("text", async (ctx) => {
     }
   }
 });
-
 // Inicia el bot
 bot.launch();
